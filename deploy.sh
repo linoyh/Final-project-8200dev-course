4c61aa3c0c0e067f2b7ea0121264e9a6321973d2
Linoy Hevron <linoylinoy@gmail.com>
2022-11-06 10:17:43 +0200

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,20 +26,23 @@ scp -o StrictHostKeyChecking=no -r "$JENKINS_PIPELINE_WORKSPACE" ec2-user@test:~
 # copy the .env.py file (I created manually becuse I have not upload it to git) to the project dir
 # bring the application up
 # the EOF enable run multiple commands via ssh in the remote server
-#"cp .env.py final-project-8200dev/ && cd /home/ec2-user/final-project-8200dev/ && docker-compose up --build && curl http://127.0.0.1:5000"
-ssh -o StrictHostKeyChecking=no ec2-user@${machine} << 'EOF'
-    cp .env.py final-project-8200dev/
-    cd /home/ec2-user/final-project-8200dev/
-    docker-compose up --build -d
-    sleep 15
-    curl http://127.0.0.1:5000
-EOF
+ssh -o StrictHostKeyChecking=no ec2-user@${machine} "cp .env.py final-project-8200dev/ && cd /home/ec2-user/final-project-8200dev/ && docker-compose up --build -d && curl http://127.0.0.1:5000"
+
+#<< 'EOF'
+#    cp .env.py final-project-8200dev/
+#    cd /home/ec2-user/final-project-8200dev/
+#    docker-compose up --build -d
+#    sleep 15
+#    curl http://127.0.0.1:5000
+#EOF
 
 echo "Deploying to $machine server succedded"
 
 #check if the current server is test. if does run test.sh script on the test server
 if [ $machine == "test" ]
 then
-  ssh -i "${SECRET_KEY}" -o StrictHostKeyChecking=no ec2-user@${machine} './test.sh"
+  ssh -i "${SECRET_KEY}" -o StrictHostKeyChecking=no ec2-user@${machine} './test.sh'
+elif
 
+fi
 
