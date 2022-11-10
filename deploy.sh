4c28aa48372e150eafc11141bdf923b9a512e379
Linoy Hevron <linoylinoy@gmail.com>
2022-11-10 14:48:57 +0200

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -30,11 +30,11 @@ scp -o StrictHostKeyChecking=no -r "$JENKINS_PIPELINE_WORKSPACE" ec2-user@${MACH
 # docker-compose up --no-build -d
 #  docker-compose up --build -d
 #  docker-compose up --build -d
+
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} << 'EOF'
   cp .env.py final-project-8200dev/
   cd /home/ec2-user/final-project-8200dev/
-  docker pull 6419/attendance_app_bynet:latest
-  docker-compose up --no-build -d
+  docker-compose up --build -d
   sleep 20
   if [ "$MACHINE" == "test" ];
   then
