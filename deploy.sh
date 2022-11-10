3c8d5a1eba1f2a0f0fc0f59bcb959997b50091f7
Linoy Hevron <linoylinoy@gmail.com>
2022-11-10 15:37:48 +0200

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -34,8 +34,8 @@ scp -o StrictHostKeyChecking=no -r "$JENKINS_PIPELINE_WORKSPACE" ec2-user@${MACH
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} << 'EOF'
   cp .env.py final-project-8200dev/
   cd /home/ec2-user/final-project-8200dev/
-  docker pull 6419/attendance_app_bynet:latest
-  docker-compose up --no-build -d  sleep 20
+  docker-compose up --build -d
+  sleep 20
   if [ "$MACHINE" == "test" ];
   then
       HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
