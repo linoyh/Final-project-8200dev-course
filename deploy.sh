0e2d48c16eaa7c0ad1abd93dee12fb1c8333b430
Linoy Hevron <linoylinoy@gmail.com>
2022-11-11 12:42:59 +0200

    remove comments in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,27 +21,21 @@ scp -o StrictHostKeyChecking=no -r "$JENKINS_PIPELINE_WORKSPACE" ec2-user@${MACH
 
 #ssh to the $machine (test ot prod):
 # copy the .env.py file (I created manually becuse I have not upload it to git) to the project dir
+# pull docker image from docker hub
 # bring the application up
-# the EOF enable run multiple commands via ssh in the remote server
-#"cp .env.py final-project-8200dev/ && cd /home/ec2-user/final-project-8200dev/ && docker-compose up --build -d && curl http://127.0.0.1:5000"
-
-
-#docker pull 6419/attendance_app_bynet:latest
-# docker-compose up --no-build -d
-#  docker-compose up --build -d
-#  docker-compose up --build -d
-#  #  HTTP=`curl -I "http://127.0.0.1:5000"`
 
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cp .env.py final-project-8200dev/ && cd /home/ec2-user/final-project-8200dev/ && docker pull 6419/attendance_app_bynet:latest && docker-compose up -d --no-build && sleep 80 && docker container ls -a"
 
+# check wether the server is test if does run the check statuss
 if [ "$MACHINE" == "test" ];
 then
-    #echo "Testing"
+    echo "Testing"
     ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "curl -I "http://127.0.0.1:5000""
 fi
 
 echo "Deploying to $MACHINE server succedded"
 
+
+#cleanup the server- test / prod
 echo "Stating cleanup in $MACHINE"
-# && docker rmi 6419/attendance_app_bynet 
-#ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/final-project-8200dev/ && docker-compose down && docker volume prune -f && cd .. && rm -rf final-project-8200dev/"
+#ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/final-project-8200dev/ && docker-compose down && docker volume prune -f && docker rmi 6419/attendance_app_bynet && cd .. && rm -rf final-project-8200dev/"
